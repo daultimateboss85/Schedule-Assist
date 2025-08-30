07f9173fb0bea82c004b43edc1c1fdaf06fad322
judah <judahadefuye1@outlook.com>
2025-08-30 11:22:27 +0100

    Test stop tracking changes to file

--- jobs/scheduler.py
+++ jobs/scheduler.py
@@ -4,7 +4,7 @@ from jobs.jobs import job
 
 def scheduler():
         #schedule.every(5).seconds.do(job)
-        schedule.every().hour.at(":00").do(job)
+        #schedule.every().hour.at(":00").do(job)
 
         while True:
             schedule.run_pending()
